be16a60b4a0e3c340e1fb7c96d89b41390ee4974
Rajsapale <rajsapale555@gmail.com>
2025-01-19 10:28:19 +0000

    variables  basics done

---
+++ jsBasic/Variables.js
@@ -0,0 +1,13 @@
+/* JS have three types of variables */
+
+var name = "raj";   // it can be redeclered and we can reassign value to it 
+
+name =28;
+console.log(name);  // var can be declared globle and can accessed from anywhere from code 
+
+let city="kolhapur"  // let can acceseed within the scope 
+
+const id=2;   // once we declared variable as const then we can't change its value
+
+ // id=4;   this is not allowed when we declared variable as const 
+console.log(id);
